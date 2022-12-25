d5b76422ff58923cf2a9b38dcb0900d828cc176c
aymanecoder <aymanbiti2@gmail.com>
2022-12-25 23:29:10 +0100

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'Login', component: LoginComponent},
+      {path: '', redirectTo: '/Login', pathMatch: 'full'},
+
+    ]),
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
