1a9456334d4f4c424d6ae9de9adbbb9694a04344
Yassine <Oussiyassine1@gmail.com>
2022-12-26 16:48:17 +0100

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,30 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+       MatTableModule,
+        MatPaginatorModule,
     RouterModule.forRoot([
       {path: 'Login', component: LoginComponent},
       {path: '', redirectTo: '/Login', pathMatch: 'full'},
-
+      {path: 'home', component: HomeComponent},
     ]),
   ],
 
